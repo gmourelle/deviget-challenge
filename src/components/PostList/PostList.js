3ddb10bb863ccfe9e8b09818ea9759daf9971cc2
gmourelle <guido.mourelle@gmail.com>
2020-02-17 22:51:35 -0300

    transition added

--- src/components/PostList/PostList.js
+++ src/components/PostList/PostList.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useDispatch, shallowEqual, useSelector } from 'react-redux';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import { dismissAllPost } from '../../store/actions';
 import Post from './Post';
 
@@ -12,13 +13,23 @@ const PostList = () => {
 
   return (
     <div className="posts__container">
-      {children &&
-        children.map(post => <Post key={post.data.id} post={post.data} />)}
-      <div className="posts__footer">
-        <button type="button" onClick={onDismissAll}>
-          Dismiss All
-        </button>
-      </div>
+      <TransitionGroup>
+        {children &&
+          children.map(post => (
+            <CSSTransition
+              key={post.data.id}
+              timeout={1500}
+              classNames="post-transition"
+            >
+              <Post key={post.data.id} post={post.data} />
+            </CSSTransition>
+          ))}
+        <div className="posts__footer">
+          <button type="button" onClick={onDismissAll}>
+            Dismiss All
+          </button>
+        </div>
+      </TransitionGroup>
     </div>
   );
 };
